b47f0b47c890ee4e1c065575b03a165c166aa0a5
Nikoletadaci <nikoletadacii@gmail.com>
2022-03-26 22:15:54 +0100

    Add Hello Express using app.get()

--- myApp.js
+++ myApp.js
@@ -37,4 +37,8 @@ var app = express();
 
 
  module.exports = app;
- console.log("Hello World")
+ console.log("Hello World");
+ app.get("/", function(req, res) {
+    res.send("Hello Express");
+  });
+  
