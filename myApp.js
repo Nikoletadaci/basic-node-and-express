e91203415b680de30e0e2a230df4247bd100637d
Nikoletadaci <nikoletadacii@gmail.com>
2022-03-26 22:30:12 +0100

    Add serve static assets to myApp.js

--- myApp.js
+++ myApp.js
@@ -38,7 +38,5 @@ var app = express();
 
  module.exports = app;
  console.log("Hello World");
- app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+ app.use("/public", express.static(__dirname + "/public"));
   
