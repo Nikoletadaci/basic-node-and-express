2741b8f83e73abad64f8778123ed8bbc08342f93
Nikoletadaci <nikoletadacii@gmail.com>
2022-03-27 18:19:19 +0200

    Chain middleware function

--- myApp.js
+++ myApp.js
@@ -29,7 +29,7 @@ app.use("/now", function(req, res, next){
 }, 
   function(req, res){
     var time = new Date().toString();
-    console.log('time' + time);
+    console.log('time ' + time);
     res.json({
       'time': time
     });
