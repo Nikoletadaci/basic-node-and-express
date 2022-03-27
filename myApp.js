5e86dc2937b280c8c609d3a9ca1e4b370bb2e8ae
Nikoletadaci <nikoletadacii@gmail.com>
2022-03-27 15:00:14 +0200

    Add a massage using res.json()

--- myApp.js
+++ myApp.js
@@ -42,5 +42,9 @@ var app = express();
     res.sendFile(__dirname + "/views/index.html");
   });
 
- app.use("/public", express.static(__dirname + "/public"));
+ app.get("/json", function(req, res) {
+   res.json({
+     message: "Hello json"
+   });
+ });
   
